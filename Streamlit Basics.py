412bcd53e14f1aae45f5c2e52756402e2915a922
jwolfson124 <65319207+jwolfson124@users.noreply.github.com>
2025-05-31 15:20:16 -0400

    Update Streamlit Basics.py

--- Streamlit Basics.py
+++ Streamlit Basics.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 st.title("""
-:red[Premier League Dashboard]
+:orange[Premier League Dashboard]
 """)
 st.write("This is my :green[**FIRST**] attempt at creating a dashboard using Streamlit!")
 st.write("This dashboard will contain data looking into the Premier League Seasons 2020, 2021, and 2022!")
